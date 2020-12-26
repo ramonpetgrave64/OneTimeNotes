b7f007e38d8f22796936bf30b8146e82c7d9ef20
Ramon Petgrave <ramon.petgrave64@gmail.com>
2020-12-26 01:20:35 -0500

    use gcm

--- index.js
+++ index.js
@@ -1,50 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-const internal_encoding = 'base64';
-const external_encoding = 'utf8'
-
 const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
-
+const express = require('express');
 const bodyParser = require('body-parser');
+const { assert } = require('console');
+
+const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+const port = 3000;
+app.listen(port, () => {
+  console.log(`OneTimeNotes listening on port ${port}!`)
+});
 app.get('/', (req, res) => {
   res.sendFile('/index.html' , { root : __dirname});
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-});
-
-const notes = {};
-
-function store_note(note) {
-  const key = crypto.randomBytes(32);
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-  const ciphertext = cipher.update(note, external_encoding, internal_encoding) + cipher.final(internal_encoding);
-  const material = {
-    iv: iv.toString(internal_encoding), 
-    key: key.toString(internal_encoding)
-  };
-  notes[material.iv] = ciphertext;
-  const link = Buffer.from(JSON.stringify(material)).toString(internal_encoding);
-  return link;
-}
-
-function retrieve_note(link) {
-  const material = JSON.parse(Buffer.from(link, encoding=internal_encoding));
-  const iv = Buffer.from(material.iv, encoding=internal_encoding);
-  const key = Buffer.from(material.key, encoding=internal_encoding);
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  const cyphertext = notes[material.iv];
-  const plaintext = decipher.update(cyphertext, internal_encoding, external_encoding) + decipher.final(external_encoding);
-  return plaintext;
-}
 
 app.post('/api/post', (req, res) => {
   const note = req.body.note;
@@ -54,12 +23,81 @@ app.post('/api/post', (req, res) => {
 
 app.get('/api/get/:link', (req, res) => {
   const link = req.params.link;
-  note = retrieve_note(link);
-  res.send(note);
+  try {
+    const note = retrieve_note(link);
+    res.send(note);
+  } catch (e) {
+    console.log(e);
+    res.send('Unable to retrieve this note');
+  };
 });
 
-note = 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetu';
-link = store_note(note);
+const internal_encoding = 'base64';
+const external_encoding = 'utf8'
+const algorithm = 'aes-256-gcm';
+
+const link_key = crypto.randomBytes(32);
+const link_iv = crypto.randomBytes(16);
+const notes = {};
+
+function encode(input) {
+  return Buffer.from(input, external_encoding).toString(internal_encoding);
+}
+
+function decode(output) {
+  return Buffer.from(output, internal_encoding).toString(external_encoding);
+}
+
+function encrypt(text, algorithm, key, iv) {
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const ciphertext = cipher.update(text, external_encoding, internal_encoding) 
+    + cipher.final(internal_encoding);
+  return ciphertext;
+}
+
+function decrypt(text, algorithm, key, iv) {
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  const plaintext = decipher.update(text, internal_encoding, external_encoding) 
+    // + decipher.final(external_encoding); // works with cbc, but not gcm?
+  return plaintext;
+}
+
+function store_note(note) {
+  const key = crypto.randomBytes(32);
+  const iv = crypto.randomBytes(16);
+  const ciphertext = encrypt(note, algorithm, key, iv);
+  const material = {
+    key: encode(key),
+    iv: encode(iv)
+  };
+  notes[material.iv] = ciphertext;
+  const link = encode(
+    encrypt(JSON.stringify(material), algorithm, link_key, link_iv)
+  );
+  return link;
+}
+
+function retrieve_note(link) {
+  const material = JSON.parse(
+    decrypt(decode(link), algorithm, link_key, link_iv)
+  );
+  console.log(material)
+  const key = Buffer.from(material.key, internal_encoding);
+  const iv = Buffer.from(material.iv, internal_encoding);
+  if (material.iv in notes) {
+    const cyphertext = notes[material.iv];
+    const plaintext = decrypt(cyphertext, algorithm, key, iv);
+    return plaintext;
+  } else {
+    throw new Error('This note is not indexed.');
+  }; 
+}
+
+// test
+const note = 'test1 test2 test3 test4 welcome to OneTimeNotes!';
+console.log(note)
+const link = store_note(note);
 console.log(link)
-const t = retrieve_note(link);
-console.log(t)
+const n = retrieve_note(link);
+console.log(n)
+assert(note == n);
